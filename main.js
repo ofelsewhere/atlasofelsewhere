089006c59c0298bf571fb6c5ecbdd4cd4f17d35e
mondaymood <hello@taraotto.com>
2022-12-21 21:29:56 +0700

    burger menu

--- main.js
+++ main.js
@@ -92,3 +92,16 @@ $('.slider-container-sneakpeak').slick({
     }
   ]
 });
+
+
+/////// Mobile menue /////// 
+
+const burgerButton = document.querySelector('.burger')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+function openMobileMenu() {
+  mobileMenu.classList.toggle('mobile-menu-hidden');
+  this.classList.toggle('active');
+}
+
+burgerButton.addEventListener('click', openMobileMenu)
