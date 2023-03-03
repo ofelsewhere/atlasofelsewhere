79f59a886d6e8bcfab486afa8cc12c5bb7497810
mondaymood <hello@taraotto.com>
2023-03-03 12:24:44 +0700

    fix dfm + rdl

--- main.js
+++ main.js
@@ -108,3 +108,54 @@ burgerButton.addEventListener('click', openMobileMenu)
 
 /////// Animate on scroll /////// 
 AOS.init();
+
+
+/////// Portfolio filter /////// 
+const gallery_buttons = document.querySelectorAll('#portfolio-filter a')
+    const gallery_items = document.querySelectorAll('#portfolio-grid .gallery-item')
+            
+    // Step #2. Start our filter function. In this case we called it filter gallery in case we might want to filter other things on the page as well. Also we have an (e) for the function parameter. This is the event (or thing that triggered the function) and we can pass data to function through that parameter.
+    function filterGallery(e) {
+    // Step #3. Because we were using <a> elements, we want to make sure that it doesn't link anywhere else. We can skip this step if we are using a different element.
+        e.preventDefault();
+        // make this <a> class active and remove class 'active' from any other <a> elements
+        // Step #13. Let's remove it from the others that we didn't click on
+        gallery_buttons.forEach(button => {
+            button.classList.remove('active')
+        });
+        // Step #12. At this stage we have a fully functional filter gallery
+        // but lets add just a little fancyness to the filter.
+        // When we click on a button, let's add a class to jump it up a notch
+        this.classList.add('active');
+        
+        // Step #5. Making sure it's all working good so far
+        console.log(this)
+        // Step #6. Add a variable that will pull the name of the category from this link
+        let filterVal = this.dataset.filter;
+
+        // Step #7. We need to loop over the gallery items 
+        gallery_items.forEach(item => {
+            // Step #8. Because we want a reset, lets add an if statement to see if we have clicked the all button (basically a reset button)
+            if (filterVal == 'all') {
+                // If they did, then remove hidden
+                item.classList.remove('hidden');
+            } 
+            // Step #9. If they clicked on a different button instead
+            else {
+                // Step #10. We need to see if our filterVal matches something.
+                // In this case we are seeing if the image's class list contains our filterVal  
+                if (item.classList.contains(filterVal)) {
+                    // If it does, then remove the hidden class
+                    item.classList.remove('hidden'); // show those that do have the filter
+                } else {
+                    // Step #11. If it doesn't, then add it on
+                    item.classList.add('hidden'); // hide those that don't have the filter
+                }
+            };
+            
+        })
+    }
+    // Step #4. Let's add an event listener for each of our buttons
+    gallery_buttons.forEach(button => {
+        button.addEventListener('click', filterGallery)
+    })
